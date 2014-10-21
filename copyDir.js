6b772ebe8174be859ae4531d1b2a3e8baead955c
jjunil79.kim@lge.com <jjunil79.kim@lge.com>
2014-10-21 16:04:58 +0900

    support symlink field in 'files'
    symlink intends to make symbolic links for local directories or files
    
    Enyo-DCO-1.1-Signed-off-by Junil Kim <logyourself@gmail.com>

---
+++ copyDir.js
@@ -0,0 +1,36 @@
+/*jshint node: true, strict: false, globalstrict: false */
+
+module.exports = copySrcToDst;
+
+var fs = require('fs'),
+	path = require('path'),
+	shelljs = require('shelljs'),
+	Rsync = require('rsync');
+
+function copySrcToDst(src, dst, next) {
+    try {
+        if (process.platform === 'win32') {
+            try {
+                shelljs.cp('-rf', src, dst);
+                setImmediate(next);
+            } catch(err) {
+                setImmediate(next, err);
+            }
+        } else {
+            var rsync = new Rsync();
+            rsync.flags({
+                    'a': true,
+                    'r': true,
+                    'L': true,
+                    'v': false
+                });
+            //TODO: rsync npm module can't handle the path including blank.
+            //      So, the path should be surrounded with double quotes.
+            rsync.source('"'+src+'"')
+                .destination('"'+dst+'"');
+            rsync.execute(next);
+        }
+    } catch(err) {
+        setImmediate(next, err);
+    }
+}
